b329eb3a206792fd915ad38ed785d416b7e2e0ff
= <=>
2021-05-13 17:59:02 -0700

    Basic functional game. Colors, scoring, end game, input buffer flush etc.

--- tetris.py
+++ tetris.py
@@ -4,8 +4,8 @@ import curses
 import time
 import random
 
-_HEIGHT = 30
-_WIDTH = 30
+_HEIGHT = 20
+_WIDTH = 10
 
 _BASE_TETROMINOS = [
         [
@@ -60,8 +60,9 @@ class GameBoard:
         self._width = width
         self._height = height
         self.current_piece = None
+        self.score = 0
+        self.lines = 0
         self.clearBoard()
-        assert(type(self._board[0]) == list)
 
     def clearBoard(self):
         self._board = [list('#' + ' ' * self._width + '#') for i in range(self._height)]
@@ -69,6 +70,8 @@ class GameBoard:
 
     def setPiece(self, piece):
         self.current_piece = piece
+        return self._canFit(piece)
+
 
     def movePiece(self, dx, dy, rotate):
         # This is not very efficient, but fast enough for tetris.
@@ -101,12 +104,11 @@ class GameBoard:
                 for x in range(4):
                     if self.current_piece[y][x] != ' ':
                         self._board[y + self.current_piece.y][x + self.current_piece.x + 1] = self.current_piece[y][x]
-            self.current_piece = None
 
     def getRenderableBoard(self):
         ret = []
         for i, line in enumerate(self._board):
-            if self.current_piece and self.current_piece.y <= i < self.current_piece.y + 4: #i >= self.current_piece.y and i < self.current_piece.y + 4:
+            if self.current_piece and self.current_piece.y <= i < self.current_piece.y + 4:
                 combined_line = list(line)
                 for col in range(4):
                     if self.current_piece[i - self.current_piece.y][col] != ' ':
@@ -115,6 +117,27 @@ class GameBoard:
             else:
                 ret.append(''.join(line))
         return ret
+
+    def _isLine(self, row):
+        row_data = self._board[row]
+        return all(char != ' ' for char in row_data)
+
+    def completeLines(self, start_y):
+        return [index for index in range(start_y, min(start_y + 6, _HEIGHT)) if self._isLine(index)]
+
+    def moveRowDown(self, row_index):
+        if row_index >= 0:
+            self._board[row_index + 1] = self._board[row_index].copy()
+
+    def _deleteRow(self, row_index):
+        for i in range(row_index - 1, -1, -1):
+            self.moveRowDown(i)
+
+    def deleteRows(self, rows):
+        for row_index in sorted(rows):
+            self._deleteRow(row_index)
+        self.score += 2 ** (len(rows) - 1)
+        self.lines += len(rows)
 
 
 class GamePiece:
@@ -139,8 +162,6 @@ class GamePiece:
         return GamePiece(self._piece, self.x, self.y)
 
 
-
-
 # TODO(muntasir): Precompute rotations
 def rotate(piece):
     rotated_piece = [[' ' for i in range(4)] for j in range(4)]
@@ -153,15 +174,42 @@ def rotate(piece):
 def render(stdscr, game_board):
     lines = game_board.getRenderableBoard()
     for y,line in enumerate(lines):
-        stdscr.addstr(y, 0, line)
-        #print(line)
+        for x, char in enumerate(line):
+            index = '0IOTJLSZ'.find(char)
+            if index != -1:
+                stdscr.addch(y, x, chr(9606), curses.color_pair(index))
+                stdscr.refresh()
+            elif char == ' ':
+                stdscr.addch(y, x, '.')
+            else:
+                stdscr.addch(y, x, char)
+    stdscr.addstr(10, _WIDTH + 5, 'score: %d' % game_board.score)
+    stdscr.addstr(11, _WIDTH + 5, 'lines: %d' % game_board.lines)
     stdscr.refresh()
+
+
+def flushInput(stdscr):
+    while True:
+        ch = stdscr.getch()
+        if ch == curses.ERR:
+            break
+
 
 def gameLoop(stdscr, game_board):
     stdscr.nodelay(True)
     ticks = 0
     speed = 8
+    running = True
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(7, curses.COLOR_RED, curses.COLOR_BLACK)
     while True:
+        flushInput(stdscr)
         time.sleep(0.0625)
 
         # User input
@@ -170,26 +218,27 @@ def gameLoop(stdscr, game_board):
             if ch == ord('q'):
                 break
             elif ch == curses.KEY_LEFT:
-                #game_board.current_piece.move(-1, 0)
                 game_board.movePiece(-1, 0, False)
             elif ch == curses.KEY_RIGHT:
-                #game_board.current_piece.move(1, 0)
                 game_board.movePiece(1, 0, False)
             elif ch == curses.KEY_DOWN:
-                #game_board.current_piece.move(0, 1)
                 game_board.movePiece(0, 1, False)
             elif ch == curses.KEY_UP:
-                #game_board.current_piece.move(0, 1)
                 game_board.movePiece(0, 0, True)
 
-        if ticks % speed == 0:
+        if ticks % speed == 0 and running:
             # Game logic.
             if not game_board.descend():
                 game_board.addPieceToBoard()
-                game_board.setPiece(GamePiece(_BASE_TETROMINOS[random.randrange(7)], _WIDTH // 2 - 2))
-
+                start_y = game_board.current_piece.y - 1
+                handleCompletedLines(game_board, start_y)
+                if not game_board.setPiece(GamePiece(_BASE_TETROMINOS[random.randrange(7)], _WIDTH // 2 - 2)):
+                   running = False
         # Render
-        render(stdscr, game_board)
+        if running:
+            render(stdscr, game_board)
+        else:
+           stdscr.addstr(_HEIGHT // 2, _WIDTH // 2, "Game Over")
         ticks += 1
 
 
@@ -197,13 +246,13 @@ def boundsCheck(value, maxBound, minBound=0):
     return minBound <= value < maxBound
 
 
-def hasCompletedLines(game_board, start_x):
-    pass
+def handleCompletedLines(game_board, start_y):
+    lines = game_board.completeLines(start_y)
+    game_board.deleteRows(lines)
 
-outer = None
 def main(stdscr):
     stdscr.clear()
-    board = GameBoard(30, 30)
+    board = GameBoard(_WIDTH, _HEIGHT)
     start_y = 0
     start_piece = _BASE_TETROMINOS[random.randrange(0, 7)]
     if start_piece[0] == '    ':
@@ -212,9 +261,6 @@ def main(stdscr):
     board.setPiece(piece)
     gameLoop(stdscr, board)
     time.sleep(1)
-    global outer
-    outer = board
 
 if __name__ == '__main__':
     curses.wrapper(main)
-    print(outer.getRenderableBoard())
